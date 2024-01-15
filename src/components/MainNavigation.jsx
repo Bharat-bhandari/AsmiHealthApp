bd18f1d9c66fc5f319fdfb2a3f5f409e371f01ec
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-01-15 23:23:59 +0530

    add Navbar

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -8,32 +8,35 @@ import { Link } from "react-router-dom";
 const MainNavigation = () => {
   return (
     <>
-      <nav>
-        <a href="/">
-          <img src="" alt="Asmi life Logo" />
-        </a>
+      <nav className=" w-full border-b-[1px] border-neutral-400">
+        <div className=" px-[5%] flex w-full max-w-screen-xl mx-auto justify-between h-[72px] items-center">
+          <a className="flex items-center gap-2">
+            <img className=" h-16 " src={logo} alt="Asmi life Logo" />
+            <p className=" font-semibold text-3xl text-[#333]">Asmi</p>
+          </a>
 
-        <div>
-          <ul className="list">
-            <li>
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li>
-              <Link to={"/how-it-works"}>How It Works</Link>
-            </li>
-            <li>
-              <Link to={"/about-us"}>Team</Link>
-            </li>
-            <li>
-              <Link to={"/counselors"}>Counselors</Link>
-            </li>
-            <li>
-              <Link to={"/blogs"}>Blogs</Link>
-            </li>
-            <li>
-              <Link to={"/contact-us"}>Contact Us</Link>
-            </li>
-          </ul>
+          <div>
+            <ul className=" flex gap-8 font-semibold text-neutral-900">
+              <li>
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/how-it-works"}>How It Works</Link>
+              </li>
+              <li>
+                <Link to={"/about-us"}>Team</Link>
+              </li>
+              <li>
+                <Link to={"/counselors"}>Counselors</Link>
+              </li>
+              <li>
+                <Link to={"/blogs"}>Blogs</Link>
+              </li>
+              <li>
+                <Link to={"/contact-us"}>Contact Us</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
