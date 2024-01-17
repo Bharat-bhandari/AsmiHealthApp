2e26cc3cb7a836b902f274737c4ca503b7f3b771
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-01-17 11:33:53 +0530

    add env file

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,12 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("./models/User");
+require("dotenv").config();
 
 const app = express();
+
+const mongoURI = process.env.MONGO_URI;
+const secretKey = process.env.SECRET_KEY;
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: true }));
@@ -19,27 +23,35 @@ app.get("/test", (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  const userDoc = await User.findOne({ username });
-  const passOk = bcrypt.compareSync(password, userDoc.password);
+  try {
+    const userDoc = await User.findOne({ username });
 
-  if (passOk) {
-    // logged in
+    if (!userDoc) {
+      return res.status(400).json("User not found");
+    }
 
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).json("ok");
-    });
-  } else {
-    res.status(400).json("wrong credentials");
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if (passOk) {
+      // logged in
+      const token = jwt.sign({ username, id: userDoc._id }, secretKey, {});
+      res.cookie("token", token, { httpOnly: true }).json("ok");
+    } else {
+      res.status(400).json("Wrong credentials");
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json("Internal Server Error");
   }
 });
 
 app.get("/profile", (req, res) => {
+  // Assuming you have a middleware to verify the JWT token here
   res.json(req.cookies);
 });
 
 mongoose
-  .connect("")
+  .connect(mongoURI)
   .then(() => {
     app.listen(4000, () => {
       console.log("Listening on port 4000");
